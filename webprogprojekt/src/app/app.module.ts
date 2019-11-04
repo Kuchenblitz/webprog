9262ce3c2f5ccf6d95c3db4feabceae9bbda0be5
RagingLynx <kevin1.heinz@gmail.com>
2019-11-04 10:14:44 +0100

    carousel: added component freizeitverwaltung: added component

--- webprogprojekt/src/app/app.module.ts
+++ webprogprojekt/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FreizeitverwaltungComponent } from './freizeitverwaltung/freizeitverwaltung.component';
+import { CarouselComponent } from './carousel/carousel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FreizeitverwaltungComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule
---
+++ webprogprojekt/src/app/freizeitverwaltung/freizeitverwaltung.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-freizeitverwaltung',
+  templateUrl: './freizeitverwaltung.component.html',
+  styleUrls: ['./freizeitverwaltung.component.css']
+})
+export class FreizeitverwaltungComponent implements OnInit {
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+
+}
