ad434e50ec51e0d8dd5f875ca36aaa53d33689cf
Kuchenblitz <marius.liebel@googlemail.com>
2019-11-09 17:39:39 +0100

    structure: project structure cleanup (ongoing)

---
+++ page-overview/page-overview.js
@@ -0,0 +1,54 @@
+"use strict";
+
+/**
+ * Klasse PageOverview: Stellt die Startseite der App zur Verfügung
+ */
+class PageOverview {
+    /**
+     * Konstruktor
+     * @param {App} app Zentrale Instanz der App-Klasse
+     */
+    constructor(app) {
+        this._app = app;
+    }
+
+    /**
+     * Seite anzeigen. Wird von der App-Klasse aufgerufen.
+     */
+    async show() {
+        // Anzuzeigenden Seiteninhalt nachladen
+        let html = await fetch("page-overview/page-overview.html");
+        let css = await fetch("page-overview/page-overview.css");
+
+        if (html.ok && css.ok) {
+            html = await html.text();
+            css = await css.text();
+        } else {
+            console.error("Fehler beim Laden des HTML/CSS-Inhalts");
+            return;
+        }
+
+        // Seite zur Anzeige bringen
+        let pageDom = document.createElement("div");
+        pageDom.innerHTML = html;
+
+        let mainElement = pageDom.querySelector("main");
+        let templateElement = pageDom.querySelector("#template-tile");
+        mainElement.innerHTML += html;
+
+        this._app.setPageTitle("Startseite");
+        this._app.setPageCss(css);
+        this._app.setPageHeader(pageDom.querySelector("header"));
+        this._app.setPageContent(pageDom.querySelector("main"));
+    }
+
+    
+    /**
+     * Hilfsmethode, welche den HTML-Code zur Darstellung der Kacheln auf
+     * der Startseite erzeugt.
+     *
+     * @param {HTMLElement} pageDom Wurzelelement der eingelesenen HTML-Datei
+     * mit den HTML-Templates dieser Seite.
+     */
+    
+}
