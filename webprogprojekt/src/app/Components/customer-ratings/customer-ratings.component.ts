c7521002088bcd5fc54c7948d08352fba30bfa96
Kuchenblitz <marius.liebel@googlemail.com>
2019-11-09 15:05:31 +0100

    Reinitializing commit: Ground structure created
    Angular was removed from the project due to unneccessary complexity

---
+++ webprogprojekt/src/app/Components/customer-ratings/customer-ratings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-ratings',
+  templateUrl: './customer-ratings.component.html',
+  styleUrls: ['./customer-ratings.component.css']
+})
+export class CustomerRatingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- webprogprojekt/src/app/app.module.ts
+++ webprogprojekt/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { FreizeitverwaltungComponent } from './Components/freizeitverwaltung/freizeitverwaltung.component';
 import { CarouselComponent } from './Components/carousel/carousel.component';
 import { UnterkunftComponent } from './Components/unterkunft/unterkunft.component';
+import { CustomerRatingsComponent } from './Components/customer-ratings/customer-ratings.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FreizeitverwaltungComponent,
     CarouselComponent,
-    UnterkunftComponent
+    UnterkunftComponent,
+    CustomerRatingsComponent
   ],
   imports: [
     BrowserModule
