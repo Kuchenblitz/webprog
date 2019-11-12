31b5a56d2b2bfa810e976c2cb96c64f5b2ee391f
Kevin Heinz <kevin1.heinz@gmail.com>
2019-11-12 11:47:41 +0100

    page-trip small changes

--- page-trip/page-trip.js
+++ page-trip/page-trip.js
@@ -18,8 +18,8 @@ class PageTrip {
     async show(matches) {
 
         //Bestimmung der Id des anzuzeigenden Elements
-        let url = window.location.href;
-        let res = url.split("/");
+        let pageUrl = window.location.href;
+        let res = pageUrl.split("/");
         this.activityId = res[res.length-1];
 
         // Anzuzeigenden Seiteninhalt nachladen
@@ -41,9 +41,11 @@ class PageTrip {
         this._getData(pageDom);
         this._app.setPageCss(css);
         this._app.setPageHeader(pageDom.querySelector("header"));
+
+        let teilnahmeButton = document.getElementById('teilnahme-button');
         
     }
-
+    
     /**
      * 
      * Funktion, die die benötigten Daten aus der Datenbank holt und in den DOM-Baum anhängt 
@@ -58,14 +60,19 @@ class PageTrip {
         Event.get().then(doc => {
             this._data = doc.data();
             let html = templateElement.innerHTML;
-           
+            
             html = html.replace("{DESCRIPTION}", this._data.description);
+            //html = html.replace("{ABENTEUER-RATING}", "2");
+            
             document.getElementById("header-div").innerHTML = "<h1>"+this._data.name+"</h1>";
             //html = html.replace("{NAME}", this._data.description);
-
+            
             mainElement.innerHTML += html;
             this._app.setPageContent(pageDom.querySelector("main"));
             this._app.setPageTitle("Trip: "+this._data.name)
+            document.body.style.background = "url(" + this._data.img_path + ")";
+            document.body.style.backgroundSize = "cover";
+            
 
 
         });
